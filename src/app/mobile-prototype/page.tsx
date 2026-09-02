b058c2b40f460a4386934e5003b9e4496c23a3da
nadyangkasa18-bit <nadyangkasa18@gmail.com>
2026-09-02 14:56:14 +0700

    Add isolated mobile companion prototype route

---
+++ src/app/mobile-prototype/page.tsx
@@ -0,0 +1,5 @@
+import { MobileTripCompanionPrototype } from "@/components/mobile-trip-companion-prototype";
+
+export default function MobilePrototypePage() {
+  return <MobileTripCompanionPrototype/>;
+}
